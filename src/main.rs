2589719940d4f3616ce72f1999b64c4d62d5acaa
Riccardo Casatta <riccardo@casatta.it>
2018-01-04 21:51:21 +0100

    counting by proto

--- src/main.rs
+++ src/main.rs
@@ -10,29 +10,50 @@ fn main() {
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer).unwrap();
     let mut counters : HashMap<String,u32> = HashMap::new();
+    let mut counters_per_proto : HashMap<String,u32> = HashMap::new();
     for el in buffer.lines() {
-        parse(el, &mut counters);
+        parse(el, &mut counters, &mut counters_per_proto);
     }
+    print_map_by_key(&counters);
+    print_map_by_value(&counters_per_proto);
+}
 
-    let mut counters_keys : Vec<_> = counters.keys().collect();
-    counters_keys.sort();
-    for el in counters_keys {
-        println!("{} {}",el,counters.get(el).unwrap());
+
+fn print_map_by_value(map : &HashMap<String,u32>) {
+    let mut count_vec: Vec<(&String, &u32)> = map.iter().collect();
+    count_vec.sort_by(|a, b| b.1.cmp(a.1));
+    for (a,b) in count_vec {
+        println!("{} {}", a, b);
     }
 }
 
-fn parse(el : &str, counters : &mut HashMap<String,u32>) {
+fn print_map_by_key(map : &HashMap<String,u32>) {
+    let mut map_keys : Vec<_> = map.keys().collect();
+    map_keys.sort();
+    for el in map_keys {
+        println!("{} {}", el, map.get(el).unwrap());
+    }
+    println!("---");
+}
+
+fn parse(el : &str, counters : &mut HashMap<String,u32>, counters_per_proto : &mut HashMap<String,u32>) {
     let mut x = el.split_whitespace();
     let timestamp = x.next();
     let value = x.next();
     if let (Some(timestamp),Some(value)) = (timestamp,value) {
         //println!("{} {}", timestamp, value);
-        let timestamp = timestamp.parse::<i64>().unwrap();
+        let timestamp = timestamp.parse::<i64>().expect("found non parsable timestamp");
         let date = Utc.timestamp(timestamp,0);
         let key = format!("{}{:02}",date.year(),date.month());
         //println!("{}", key);
-        let counter = counters.entry(key).or_insert(1);
+        let counter = counters.entry(key).or_insert(0);
         *counter += 1;
+
+        if value.len()>9 {
+            let proto=&value[4..10];
+            let counter_per_proto = counters_per_proto.entry(proto.to_owned()).or_insert(0);
+            *counter_per_proto += 1;
+        }
     }
 
 }
