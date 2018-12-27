d5bee233b38bc063a64a6cab3e01e36cec5f4bd7
Riccardo Casatta <riccardo@casatta.it>
2018-12-27 11:33:41 +0100

    op return last year

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,7 @@ struct Parsed {
     ym : String,
     script : String,
     is_last_month : bool,
+    is_last_year : bool,
     is_segwit : bool,
     op_ret_proto: Option<String>,
     script_size: usize,
@@ -40,6 +41,7 @@ struct Maps {
     op_ret_per_month : HashMap<String,u32>,
     op_ret_per_proto : HashMap<String,u32>,
     op_ret_per_proto_last_month : HashMap<String,u32>,
+    op_ret_per_proto_last_year : HashMap<String,u32>,
     op_ret_size : HashMap<usize,u32>,
     txo_per_month : HashMap<String,u32>,
     segwit_per_month : HashMap<String,u32>,
@@ -59,6 +61,7 @@ impl Maps {
             op_ret_per_month : HashMap::new(),
             op_ret_per_proto : HashMap::new(),
             op_ret_per_proto_last_month : HashMap::new(),
+            op_ret_per_proto_last_year : HashMap::new(),
             op_ret_size : HashMap::new(),
             txo_per_month : HashMap::new(),
             segwit_per_month : HashMap::new(),
@@ -75,7 +78,9 @@ fn run() -> Result<()> {
     let tx_template = read_template("templates/template.html");
 
     let mut maps = Maps::new();
-    let from = Utc::now() - Duration::days(30); // 1 month ago
+    let month_ago = Utc::now() - Duration::days(30); // 1 month ago
+    let year_ago = Utc::now() - Duration::days(365); // 1 year ago
+
 
     let num = num_cpus::get_physical();
     eprintln!("num_cpus {}",num);
@@ -92,7 +97,7 @@ fn run() -> Result<()> {
                 let received = receiver.recv().unwrap();
                 match received {
                     Some(value) => {
-                        if let Some(parsed) = parse_row(value, from) {
+                        if let Some(parsed) = parse_row(value, &month_ago, &year_ago) {
                             let _r = cloned_parsed_sender.send(Some(parsed));
                         };
 
@@ -139,6 +144,7 @@ fn run() -> Result<()> {
 
             let op_ret_per_proto : Serie  = print_map_by_value(&maps.op_ret_per_proto);
             let op_ret_per_proto_last_month : Serie  = print_map_by_value(&maps.op_ret_per_proto_last_month);
+            let op_ret_per_proto_last_year : Serie  = print_map_by_value(&maps.op_ret_per_proto_last_year);
             let tx_per_template = print_map_by_value(&maps.tx_per_template);
             let tx_per_template_last_month = print_map_by_value(&maps.tx_per_template_last_month);
 
@@ -152,6 +158,8 @@ fn run() -> Result<()> {
                      "op_ret_per_proto_data":op_ret_per_proto.data,
                      "op_ret_per_proto_last_month_labels":op_ret_per_proto_last_month.labels,
                      "op_ret_per_proto_last_month_data":op_ret_per_proto_last_month.data,
+                     "op_ret_per_proto_last_year_labels":op_ret_per_proto_last_year.labels,
+                     "op_ret_per_proto_last_year_data":op_ret_per_proto_last_year.data,
                      "tx_per_template_labels":tx_per_template.labels,
                      "tx_per_template_data":tx_per_template.data,
                      "tx_per_template_last_month_labels":tx_per_template_last_month.labels,
@@ -324,7 +332,7 @@ fn parse_script(script : &bitcoin::blockdata::script::Script) -> String {
 
 
 
-fn parse_row(el : String, from : DateTime<Utc>) -> Option<Parsed> {
+fn parse_row(el : String, month_ago : &DateTime<Utc>, year_ago : &DateTime<Utc>) -> Option<Parsed> {
     let mut x = el.split_whitespace();
     let timestamp = x.next();
     let value = x.next();
@@ -338,7 +346,8 @@ fn parse_row(el : String, from : DateTime<Utc>) -> Option<Parsed> {
         let script_size = script_bytes.len();
         let script = Script::from(script_bytes);
         let script = parse_script(&script);
-        let is_last_month = date > from;
+        let is_last_month = date > *month_ago;
+        let is_last_year = date > *year_ago;
         let is_segwit = value.starts_with("0014") ||  value.starts_with("0020");
 
         let op_ret_proto = if value.starts_with("6a") && value.len() > 9 {
@@ -354,6 +363,7 @@ fn parse_row(el : String, from : DateTime<Utc>) -> Option<Parsed> {
                 script,
                 op_ret_proto,
                 is_last_month,
+                is_last_year,
                 is_segwit,
                 script_size,
             }
@@ -372,6 +382,9 @@ fn update(parsed : Parsed, maps :  &mut Maps) {
     if let Some(op_ret_proto) = parsed.op_ret_proto {
         if parsed.is_last_month {
             *maps.op_ret_per_proto_last_month.entry(op_ret_proto.clone()).or_insert(0) += 1;
+        }
+        if parsed.is_last_year {
+            *maps.op_ret_per_proto_last_year.entry(op_ret_proto.clone()).or_insert(0) += 1;
         }
         *maps.op_ret_per_month.entry(parsed.ym.clone()).or_insert(0)+=1;
         *maps.op_ret_per_proto.entry(op_ret_proto).or_insert(0)+=1;
